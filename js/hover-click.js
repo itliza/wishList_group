727f6177e03ede0632c7038baa2cfbac46d29946
CarLifeovich <CarLifeovich@users.noreply.github.com>
2022-08-09 11:35:44 +0300

    small upgrade bb_card_text

--- js/hover-click.js
+++ js/hover-click.js
@@ -1,6 +1,7 @@
-const hover__click = document.querySelector('.wishlist_card');
+const hover__click = document.querySelectorAll('.wishlist_card');
 
-hover__click.addEventListener('click', function(){
-    header.classList.add('.wishlist_card_hov');
-
+hover__click.forEach(hoverItem, ()=>{
+    hoverItem.addEventListener('click', ()=>{
+        hoverItem.classList.add('wishlist_card_hov')
+    })
 })
