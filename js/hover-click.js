7167e1d2477af564ab6d3eca34d2368440cadb77
Liza Romaniuk <elizaveta.romaniuk@gmail.com>
2022-08-11 11:29:51 +0300

    last updates

--- js/hover-click.js
+++
@@ -1,7 +0,0 @@
-const hover__click = document.querySelectorAll('.wishlist_card');
-
-hover__click.forEach(hoverItem, ()=>{
-    hoverItem.addEventListener('click', ()=>{
-        hoverItem.classList.add('wishlist_card_hov')
-    })
-})
